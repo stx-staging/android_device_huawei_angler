bbf762e3a08aab3f8de6dd7fc8c24ad577108830
Robert Shih <robertshih@google.com>
2019-11-25 22:25:41 -0800

    angler: Enable v1.3 drm plugins
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: I7453b035ca29c7ee889d03de0d55e6d947b3cb77
    Signed-off-by: electimon <electimon@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -46,7 +46,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint Sensor
 PRODUCT_PACKAGES += \
